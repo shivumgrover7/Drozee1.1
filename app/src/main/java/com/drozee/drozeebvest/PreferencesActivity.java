ed7c9fda5dce2be481f6ece0de86494383f6db26
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2018-10-09 00:50:19 +0530

    Add functionality to write Books to Firebase

--- app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
+++ app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
@@ -11,17 +11,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
 
 public class PreferencesActivity extends AppCompatActivity {
     ArrayList<String> pref = new ArrayList<String>();
     RecyclerViewAdapter viewAdapter = new RecyclerViewAdapter(this, pref);
+    FirebaseDatabase mFirebaseDatabase;
+    DatabaseReference mListReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
-
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mListReference = mFirebaseDatabase.getReference();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.prefRV);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
@@ -50,6 +57,10 @@ public class PreferencesActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if(viewAdapter.getItemCount() >= 5)
                 {
+              if(viewAdapter.getItemCount() == 1)
+                        mListReference.child("Books").push().setValue(pref);
+                    else
+                        mListReference.child("Books").setValue(pref);
 
                 }
                 else
@@ -64,47 +75,7 @@ public class PreferencesActivity extends AppCompatActivity {
 
 
         //Set Custom Font
-        //TextView prefTV = (TextView) findViewById(R.id.prefTV);
-        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "CaviarDreams_Bold.ttf");
-        //prefTV.setTypeface(myCustomFont);
-        //Find Submit Button
-//        Button submitPref = (Button)findViewById(R.id.prefSubmitbtn);
-//        final ArrayList<String> preferences = new ArrayList<String>();
-//        submitPref.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                    EditText editText1 = (EditText)findViewById(R.id.ET1);
-//                    preferences.add(editText1.getText().toString());
-//
-//                    EditText editText2 = (EditText)findViewById(R.id.ET2);
-//                    preferences.add(editText2.getText().toString());
-//
-//                    EditText editText3 = (EditText)findViewById(R.id.ET3);
-//                    preferences.add(editText3.getText().toString());
-//
-//                    EditText editText4 = (EditText)findViewById(R.id.ET4);
-//                    preferences.add(editText4.getText().toString());
-//
-//                    EditText editText5 = (EditText)findViewById(R.id.ET5);
-//                    preferences.add(editText5.getText().toString());
-//
-//                    EditText editText6 = (EditText)findViewById(R.id.ET6);
-//                    preferences.add(editText6.getText().toString());
-//
-//                    EditText editText7 = (EditText)findViewById(R.id.ET7);
-//                    preferences.add(editText7.getText().toString());
-//
-//                    EditText editText8 = (EditText)findViewById(R.id.ET8);
-//                    preferences.add(editText8.getText().toString());
-//
-//                    EditText editText9 = (EditText)findViewById(R.id.ET9);
-//                    preferences.add(editText9.getText().toString());
-//
-//                    EditText editText10 = (EditText)findViewById(R.id.ET10);
-//                    preferences.add(editText10.getText().toString());
-//            }
-//        });
+        //Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "CaviarDreams_Bold.ttf");
 
     }
 }
