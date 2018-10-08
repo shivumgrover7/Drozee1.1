39988064bdd82c38fb3f41a684ebfb8d9f63ecaa
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2018-10-08 16:48:17 +0530

    Edit PreferencesActivity

--- app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
+++ app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
@@ -1,4 +1,13 @@
 package com.drozee.drozeebvest;
 
-public class PreferencesActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PreferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preferences);
+    }
 }
