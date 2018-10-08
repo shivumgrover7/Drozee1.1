514eee65b81f370ecc36046c2b593665260a2695
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2018-10-08 17:56:54 +0530

    Edit PreferenceActivity to add ArrayList

--- app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
+++ app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
@@ -1,7 +1,14 @@
 package com.drozee.drozeebvest;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class PreferencesActivity extends AppCompatActivity {
 
@@ -9,5 +16,48 @@ public class PreferencesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
+        //Set Custom Font
+        TextView prefTV = (TextView) findViewById(R.id.prefTV);
+        Typeface myCustomFont = Typeface.createFromAsset(getAssets(), "CaviarDreams_Bold.ttf");
+        prefTV.setTypeface(myCustomFont);
+        //Find Submit Button
+        Button submitPref = (Button)findViewById(R.id.prefSubmitbtn);
+        final ArrayList<String> preferences = new ArrayList<String>();
+        submitPref.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                    EditText editText1 = (EditText)findViewById(R.id.ET1);
+                    preferences.add(editText1.getText().toString());
+
+                    EditText editText2 = (EditText)findViewById(R.id.ET2);
+                    preferences.add(editText2.getText().toString());
+
+                    EditText editText3 = (EditText)findViewById(R.id.ET3);
+                    preferences.add(editText3.getText().toString());
+
+                    EditText editText4 = (EditText)findViewById(R.id.ET4);
+                    preferences.add(editText4.getText().toString());
+
+                    EditText editText5 = (EditText)findViewById(R.id.ET5);
+                    preferences.add(editText5.getText().toString());
+
+                    EditText editText6 = (EditText)findViewById(R.id.ET6);
+                    preferences.add(editText6.getText().toString());
+
+                    EditText editText7 = (EditText)findViewById(R.id.ET7);
+                    preferences.add(editText7.getText().toString());
+
+                    EditText editText8 = (EditText)findViewById(R.id.ET8);
+                    preferences.add(editText8.getText().toString());
+
+                    EditText editText9 = (EditText)findViewById(R.id.ET9);
+                    preferences.add(editText9.getText().toString());
+
+                    EditText editText10 = (EditText)findViewById(R.id.ET10);
+                    preferences.add(editText10.getText().toString());
+            }
+        });
+
     }
 }
