3a5d3f70260afb3019a4cf02ffef737a7e03116c
Divyanshu Sharma <sharmadivyanshu1709@gmail.com>
2018-10-09 21:33:33 +0530

    Fix IDLoginActivity and add Read from Firebase

--- app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
+++ app/src/main/java/com/drozee/drozeebvest/PreferencesActivity.java
@@ -1,39 +1,93 @@
 package com.drozee.drozeebvest;
 
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PreferencesActivity extends AppCompatActivity {
     ArrayList<String> pref = new ArrayList<String>();
     RecyclerViewAdapter viewAdapter = new RecyclerViewAdapter(this, pref);
     FirebaseDatabase mFirebaseDatabase;
     DatabaseReference mListReference;
+    DatabaseReference mBooksReference;
+    ChildEventListener mChildEventListener;
 
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_preferences);
         mFirebaseDatabase = FirebaseDatabase.getInstance();
-        mListReference = mFirebaseDatabase.getReference();
-        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.prefRV);
+        String uid = user.getUid();
+        mListReference = mFirebaseDatabase.getReference().child("Books").child(uid);
+        mBooksReference = mListReference.child("Books").child("uid");
+        final RecyclerView recyclerView = (RecyclerView) findViewById(R.id.prefRV);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         recyclerView.setLayoutManager(mLayoutManager);
         recyclerView.setAdapter(viewAdapter);
 
+        mChildEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                    List<String> lst = new ArrayList<String>(); // Result will be holded Here
+  //                  for(DataSnapshot dsp : dataSnapshot.getChildren()){
+                        lst.add(String.valueOf(dataSnapshot.getValue()));
+                        for(int z = 0; z < lst.size(); z++)
+                        {
+                            Log.v("Details(Child): ", lst.get(z));
+                            String[] atom = lst.get(z).split(",");
+                            pref.add(atom[0] + "," + atom[1]);
+                            for(int i = 0; i < pref.size(); i++)
+                                Log.v("PrefList: ", pref.get(i));
+
+                        }
+            }
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+        mListReference.addChildEventListener(mChildEventListener);
         Button add = (Button)findViewById(R.id.addBTN);
         final Button submit = (Button)findViewById(R.id.submitBTN);
         final EditText prefET = (EditText)findViewById(R.id.prefET);
@@ -50,29 +104,16 @@ public class PreferencesActivity extends AppCompatActivity {
                     pref.add(prefET.getText().toString() + "," + authET.getText().toString());
                     prefET.setText("");
                     authET.setText("");
-                    viewAdapter.notifyDataSetChanged();
-                    if (viewAdapter.getItemCount() >= 5)
+                    if(viewAdapter.getItemCount() >= 5)
                     {
-                        submit.setBackgroundResource(R.drawable.edittext);
-                    }
-                }
-            }
-        });
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(viewAdapter.getItemCount() >= 5)
-                {
-              if(viewAdapter.getItemCount() == 1)
-                        mListReference.child("Books").push().setValue(pref);
-                    else
-                        mListReference.child("Books").setValue(pref);
-                    Toast.makeText(PreferencesActivity.this, "Uploading Data", Toast.LENGTH_SHORT).show();
+                        if(viewAdapter.getItemCount() == 1)
+                            mListReference.setValue(pref);
+                        else
+                            mListReference.setValue(pref);
+                            Toast.makeText(PreferencesActivity.this, "Uploading Data", Toast.LENGTH_SHORT).show();
 
-                }
-                else
-                {
-                    Toast.makeText(PreferencesActivity.this, "Please enter at least 5 books", Toast.LENGTH_SHORT).show();
+                    }
+                    viewAdapter.notifyDataSetChanged();
                 }
             }
         });
